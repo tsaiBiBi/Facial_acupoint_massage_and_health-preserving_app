a88556f82b7e6e0386c61fed17ef55fedec2214e
Kiwi <steven97xup60116@gmail.com>
2020-08-18 00:23:35 +0800

    login/register

--- Acupuncture/app/src/main/java/com/example/acupuncture/LoginActivity.java
+++ Acupuncture/app/src/main/java/com/example/acupuncture/LoginActivity.java
@@ -4,20 +4,58 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.webservice.User;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_back , btn_login;
+    public static EditText et_account , et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // edittext
+        et_account  = (EditText) findViewById(R.id.eT_account_log);
+        et_password = (EditText) findViewById(R.id.eT_password_log);
+
+        // button
+        btn_login = (Button) findViewById(R.id.btn_log_ok);
+        btn_back  = (Button) findViewById(R.id.btn_log_back);
+
+
+        // 判斷空值及登入
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String faccount  = et_account.getText().toString();
+                String fpassword = et_password.getText().toString();
+                if(!chk_null(faccount , fpassword)) {
+                    User.login(LoginActivity.this , faccount , fpassword);
+                }
+            }
+        });
+
         // 關閉當前頁面
-        Button back = (Button) findViewById(R.id.bt_log_back);
-        back.setOnClickListener(new View.OnClickListener() {
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    // 確認是否有欄位為空值
+    private boolean chk_null(String faccount, String fpassword) {
+        Boolean is_null = false;
+        if(faccount.isEmpty() || fpassword.isEmpty()) {
+            Toast.makeText(LoginActivity.this ,"請填完所有欄位" , Toast.LENGTH_LONG).show();
+            is_null = true;
+        }
+        return is_null;
     }
 }

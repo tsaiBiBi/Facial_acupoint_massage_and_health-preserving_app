1f8b7a4335fb6903eaf3c994480ca253cc0293fe
Rocio-Liu <a3868111@gmail.com>
2020-08-27 03:39:14 +0800

    v1.2

--- Acupuncture/app/src/main/java/com/example/acupuncture/EditActivity.java
+++ Acupuncture/app/src/main/java/com/example/acupuncture/EditActivity.java
@@ -1,21 +1,17 @@
 package com.example.acupuncture;
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+public class EditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_edit);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -23,17 +19,6 @@ public class ProfileActivity extends AppCompatActivity {
             // 設置返回鍵
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
-        // 點擊編輯按鈕
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_edit);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(ProfileActivity.this, EditActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override

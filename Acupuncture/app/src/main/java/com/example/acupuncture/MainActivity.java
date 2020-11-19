76fef89f66858e3b775667e48d9347105d242872
tiffany92257 <tingting2733682@gmail.com>
2020-11-19 18:15:21 +0800

    1.4.3

--- Acupuncture/app/src/main/java/com/example/acupuncture/MainActivity.java
+++ Acupuncture/app/src/main/java/com/example/acupuncture/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.acupuncture;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 
 import com.example.dataclass.Urls;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -24,6 +31,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.dataclass.SharedPrefManager;
 
 import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -82,10 +90,9 @@ public class MainActivity extends AppCompatActivity {
                 // 各選項點擊事件
                 switch (menuItem.getItemId()) {
                     case R.id.nav_profile:
-                        if(sharedprefmanager.get_user_detail().get(SharedPrefManager.ID) == null) {
+                        if (sharedprefmanager.get_user_detail().get(SharedPrefManager.ID) == null) {
                             Toast.makeText(MainActivity.this, "訪客無法使用此功能", Toast.LENGTH_LONG).show();
-                        }
-                        else {
+                        } else {
                             Intent intent = new Intent();
                             intent.setClass(MainActivity.this, ProfileActivity.class);
                             startActivity(intent);
@@ -114,22 +121,21 @@ public class MainActivity extends AppCompatActivity {
     // 辨別使用者為會員或者訪客
     private void chk_usr_identity(TextView nav_user_name) {
         sharedprefmanager = new SharedPrefManager(this);
-        if(sharedprefmanager.chk_login()) {
+        if (sharedprefmanager.chk_login()) {
             getPrefs();
-        }
-        else {
+        } else {
             nav_user_name.setText("guest");
         }
     }
 
     // 設定使用者名稱
     private void getPrefs() {
-        HashMap<String , String> user = sharedprefmanager.get_user_detail();
+        HashMap<String, String> user = sharedprefmanager.get_user_detail();
         String name = user.get(SharedPrefManager.NAME) == null ? "遊客" : user.get(SharedPrefManager.NAME);
         String img = user.get(SharedPrefManager.IMG) == null ? "遊客" : user.get(SharedPrefManager.IMG);
         nav_user_name.setText(name);
         img_url = Urls.self_img_url + img;
-        Func.set_user_image(MainActivity.this , img_url , nav_user_image);
+        Func.set_user_image(MainActivity.this, img_url, nav_user_image);
     }
 
     public String set_gender_img() {
@@ -143,6 +149,39 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         getPrefs();
         set_gender_img();
-        Func.set_user_image_no_cache(MainActivity.this , img_url , nav_user_image);
+        Func.set_user_image_no_cache(MainActivity.this, img_url, nav_user_image);
     }
+
+//    @Override
+//    protected void onNewIntent(Intent intent) {
+//        //點擊通知欄到主Activity時 會執行這個方法
+//        super.onNewIntent(intent);
+//
+//        Log.i("FragmentTest", "到底去哪了");
+//        getNotify(intent);
+//        setIntent(intent);
+//    }
+//
+//    private void getNotify(Intent intent) {
+//        //拿到參數後 首先判斷是不是爲空
+//        Log.i("FragmentTest", "有進來這嗎");
+//        if(!intent.getStringExtra("toValue").isEmpty()) {
+//            String toFragment = intent.getStringExtra("toValue");
+//            FragmentManager fragmentManager = getSupportFragmentManager();
+//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//            // If menuFragment is defined, then this activity was launched with a fragment selection
+//            Log.i("FragmentTest", "有進來嗎" + toFragment);
+//
+//            // Here we can decide what do to -- perhaps load other parameters from the intent extras such as IDs, etc
+//            if (toFragment.equals("faceFragment")) {
+//                faceFragment faceFragment = new faceFragment();
+//                fragmentTransaction.replace(R.id.nav_host_fragment, faceFragment);
+//                Log.i("FragmentTest", "有進來哦");
+//            }
+//        }
+//        //做完操作以後必須將toValue的值初始化
+//        intent.putExtra("toValue", "");
+//        super.onNewIntent(intent);
+//    }
 }

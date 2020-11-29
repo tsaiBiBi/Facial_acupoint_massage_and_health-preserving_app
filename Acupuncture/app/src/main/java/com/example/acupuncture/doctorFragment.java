924c9ed88e07ff109bde7e14f79298356a603a75
lulala88 <asdfgh61327@gmail.com>
2020-11-30 02:05:39 +0800

    dotorfrahment add google map

--- Acupuncture/app/src/main/java/com/example/acupuncture/doctorFragment.java
+++ Acupuncture/app/src/main/java/com/example/acupuncture/doctorFragment.java
@@ -2,6 +2,9 @@ package com.example.acupuncture;
 
 import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -17,13 +20,19 @@ import android.widget.Toast;
 
 import com.example.dataclass.Pressed;
 import com.example.webservice.User;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class doctorFragment extends Fragment {
+public class doctorFragment extends Fragment implements OnMapReadyCallback{
 
+    GoogleMap map;
+    MapView mapView;
     public static List<Pressed> recordweek = new ArrayList<>();
     public List<String> disease_tmp = new ArrayList<String>();
     private static String[] fmyp = new String[]{"眼睛保健","臉部美容","緩解頭痛", "改善鼻子不適",
@@ -34,6 +43,7 @@ public class doctorFragment extends Fragment {
     private ArrayAdapter<String> adapter;
     private ArrayAdapter<String> adapter2;
     private String[][] division = new String[][]{{"眼科"}, {"家醫科", "神經科", "精神科"}};
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -97,7 +107,7 @@ public class doctorFragment extends Fragment {
             }
             //為了要讓他的長度不要無限增大
             //七天超過四次在這裡判斷的
-            if (disease_tmpcount[i] > 2){
+            if (disease_tmpcount[i] > 1){
                 if (disease_tmp.contains(fmyp[i])){
                 }else {
                     disease_tmp.add(fmyp[i]);
@@ -105,6 +115,23 @@ public class doctorFragment extends Fragment {
             }
         }
         return;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = view.findViewById(R.id.mapView);
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        map = googleMap;
+
     }
 }
 
